6d6b042cc3cc11b784f6d02b135175941547c92c
0HungryDeveloper <hungrystudentdev@gmail.com>
2022-08-29 15:28:48 -0500

    Set variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { ControlsComponent } from './components/controls/controls.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		TableComponent,
+		ControlsComponent,
+		HeaderComponent,
+		FooterComponent,
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		HttpClientModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
